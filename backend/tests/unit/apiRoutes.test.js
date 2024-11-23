25d335ae8a57d20b8fac49b32816615010862b15
marouaneltf <marouaneltaifi@gmail.com>
2024-11-23 02:27:16 +0100

    s

--- backend/tests/unit/apiRoutes.test.js
+++ backend/tests/unit/apiRoutes.test.js
@@ -2,7 +2,7 @@ import request from 'supertest';
 import app from '../index';
 
 test('GET /api/employees returns employee list', async () => {
-  const res = await request(app).get('http://localhost:5000/employees');
+  const res = await request(app).get('/employees');
   expect(res.statusCode).toBe(200);
   expect(Array.isArray(res.body)).toBe(true);
 });
